848d20e8a4661eb723673f1bd94f8ecd10e307b9
travelmate <a@a.a>
2022-05-21 21:27:00 +0200

    working multi capture; todos for Bartek

--- Pawn.py
+++ Pawn.py
@@ -4,7 +4,7 @@ class Pawn:
 		self.color:str = color
 		self.position = position
 		self.is_queen:bool = False
-		self.is_removed:bool = False
+		self.is_captured:bool = False
 		self.black_pawn = 'b'
 		self.black_king = 'B'
 		self.white_pawn = 'w'
@@ -14,7 +14,10 @@ class Pawn:
 		print('Color: ' , self.color)
 		print(f'Position: {self.position} ')
 		print('Queen: ' , self.is_queen)
+        # print("is_captured",is)
 
 	def color_letter(self):
+		if self.is_queen:
+			return self.white_king if self.color == 'white' else self.black_king
 		return self.white_pawn if self.color == 'white' else self.black_pawn
 		
--- app.py
+++ app.py
@@ -29,13 +29,26 @@ if __name__ == "__main__":
     sim2 = simulation(
         [Pawn("black", [5, 2]), Pawn("black", [5, 4]), Pawn("black", [3, 2]), Pawn("black", [3, 4])]
     )
-    print_board(sim2)
-    # get_available_captures(sim,sim[0])
-    c = get_captures(sim2, Pawn("white", [6, 3]))
+    sim3 = simulation(
+        [Pawn("black", [5, 2]),Pawn("black", [5, 6]), Pawn("black", [5, 4]), Pawn("black", [3, 2]), Pawn("black", [3, 4])]
+    )
+    sim4 = simulation(
+        [Pawn("black", [4, 3]), Pawn("black", [1, 2]), Pawn("black", [5, 6])]
+    )
+    sim5 = simulation(
+        [Pawn("black", [4, 3]), Pawn("black", [3, 6]), Pawn("black", [1, 4])]
+    )
+        # get_available_captures(sim,sim[0])
+    queen = Pawn("white", [6, 1])
+    queen.is_queen = True
+    print_board(sim5+[queen])
+    # _, c = get_available_captures(sim4,queen)
+    # print(c)
+    c = get_captures(sim5, queen)
     # for k in range(len(b)):
     #     print("b",b[k])
     # pass
-    print("c", c[0])
+    print("c", c)
     # pawns = initialize_board()
     # g: GameTree = GameTree(depth=6)
     # print(6)
@@ -58,3 +71,64 @@ if __name__ == "__main__":
     # print_board(pawns)
     # for x in pawns:
     #     x.print()
+
+
+    ## interfeejs 
+    #flatten
+    # is_capture_move, moves = get_all_possible_moves (color) -> zwraca tylko bicia albo tylko ruchy
+    # board = create_new_board_from_move(board,move)
+    # evaluate(board) ->  I choose move nr 3 ([0,2])
+    # promotion (pawn)
+    # game_status(board) ->win/lose/draw
+    # evaluate(board) 
+    # perform_capture_move
+    # perform_positional_move
+    
+
+	# def move_limit_reached(self):
+	# 	return self.moves_since_last_capture >= self.consecutive_noncapture_move_limit
+
+	# def is_over(self):
+	# 	return self.move_limit_reached() or not self.get_possible_moves()
+
+	# def get_winner(self):
+	# 	if self.whose_turn() == 1 and not self.board.count_movable_player_pieces(1):
+	# 		return 2
+	# 	elif self.whose_turn() == 2 and not self.board.count_movable_player_pieces(2):
+	# 		return 1
+	# 	else:
+	# 		return None
+
+
+    # my_rating = 0
+    #     opponents_rating = 0
+    #     size=len(self.board)
+    #     for i in range(size):
+    #         for j in range((i + 1) % 2, size, 2):
+    #             piece = self.board[i][j]
+    #             if (piece != 0): # field is not empty
+    #                 zone=self.check_zone(size, (piece.col, piece.row))
+    #                 if piece.color == WHITE: # this is my piece
+    #                     if piece.queen:
+    #                         my_rating += zone * 5 # this is my queen
+    #                     else:
+    #                         my_rating += zone * 1 # this is my man
+    #                 else:
+    #                     if piece.queen:
+    #                         opponents_rating += zone * 5 # this is opponent's queen
+    #                     else:
+    #                         opponents_rating += zone * 1  
+    #     # print("Type your message here, you will see it in the log window\n");
+    #     return opponents_rating - my_rating
+    
+    # def check_zone(self, size, field):
+    #     y, x = field
+    #     zone = 1
+        
+    #     if y > 0 and x > 0 and y < size - 1 and x < size - 1:
+    #         zone = 2
+        
+    #     if y > 1 and x > 1 and y < size - 2 and x < size - 2:
+    #         zone = 3
+        
+    #     return zone
